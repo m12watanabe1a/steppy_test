81819e86023bb36c3d486c5a43cf58e8a7663e2e
m12watanabe1a <m12watanabe1a@gmail.com>
2021-03-03 23:39:54 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@ namespace plt = matplotlibcpp;
 
 int main(void)
 {
-  int32_t target_pos = 1000;
+  int32_t target_pos = 10000;
   int32_t current_pos = 0;
 
-  const uint16_t delay_max = 0b0001111111111111; // 8191
-  const uint16_t delay_min = 0b0000000001111111; // 127
+  const uint16_t delay_max = 8000; // 8191
+  const uint16_t delay_min = 100;  // 127
 
-  const uint16_t speedup_steps = 200;
-  const uint16_t speeddown_steps = 200;
+  const uint16_t speedup_steps = 500;
+  const uint16_t speeddown_steps = 500;
 
   static uint16_t last_delay_microsec = delay_max;
   static uint32_t step_counter = 1; // counter should start from 1
   uint8_t stepping_state = 0;
-  uint8_t acc = 2;
+  uint8_t acc = 4;
   uint16_t delay_microsec = delay_max;
 
   std::vector<uint16_t> C_list;
@@ -29,15 +29,15 @@ int main(void)
 
   while (target_pos != current_pos)
   {
-    if (delay_microsec == delay_max)
+    if (delay_microsec >= delay_max)
     {
       step_counter = 1; // counter should start from 1
     }
 
     int32_t diff = target_pos - current_pos;
     //delay50:1 m/s, delay250:0.2 m/s
-    C_list.push_back(delay_microsec);
-    C_inv.push_back(1.0 / delay_microsec);
+    C_list.push_back(delay_microsec / acc);
+    C_inv.push_back(1.0 / delay_microsec *acc);
     int32_t sum_of_elems = 0;
     for (auto &n : C_list)
       sum_of_elems += n;
@@ -49,7 +49,7 @@ int main(void)
         : current_pos--;
 
     // update delay time
-    if (abs(diff)  < speeddown_steps)
+    if (abs(diff) < speeddown_steps)
     {
       // speed down
       speeddown_cnt++;
@@ -65,9 +65,16 @@ int main(void)
   }
   std::cout << speedup_cnt << std::endl;
   std::cout << speeddown_cnt << std::endl;
+  std::cout << step_counter << std::endl;
+  float v_max = 1.0 / delay_min;
 
   plt::figure();
+  plt::grid(true);
+  // plt::xlim(0.0, 0.5e6);
   plt::plot(t_list, C_inv);
+  std::vector<int32_t> t = {t_list.front(), t_list.back()};
+  std::vector<float> v_max_vec = {v_max, v_max};
+  plt::plot(t, v_max_vec);
   plt::show();
 
   return EXIT_SUCCESS;
